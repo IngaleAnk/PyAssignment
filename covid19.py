83c5a06e1adc93169774f569890de08180c35d71
Ankita ingale <101617613+IngaleAnk@users.noreply.github.com>
2024-04-02 18:41:23 +0530

    Create covid19.py

---
+++ covid19.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL for scraping data
+url = 'https://data.covid19india.org/v4/min/timeseries.min.json'
+page = requests.get(url)
+print(page)
+soup= BeautifulSoup(page.content, 'html.parser')
+print(soup)
